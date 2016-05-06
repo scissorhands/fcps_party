1aa132adcce729e1a9ba5190b7ff0224867672fb
scissorhands <edward_beats>
2016-05-06 16:12:08 -0500

    validate promo minimum invited

--- application/controllers/Students.php
+++ application/controllers/Students.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Students extends CI_Controller {
-	public $promos;
+	public $promos_arr;
 
 	public function __construct()
 	{
@@ -12,7 +12,7 @@ class Students extends CI_Controller {
 		$this->load->model('promo_orders_model', 'promo_orders');
 		$this->load->model('utilities_db_model', 'utilities_db');
 		$promos = $this->promos->get_all();
-		$this->promos = parse_to_key_vals($promos, "id", "name");
+		$this->promos_arr = parse_to_key_vals($promos, "id", "name");
 	}
 
 	public function index()
@@ -28,10 +28,10 @@ class Students extends CI_Controller {
 		$this->validate_order();
 		$student = $this->students->get($student_id);
 		$this->load->view("template/loader", array(
-			"title" => "New Promo Order",
+			"title" => "Nuevo pedido",
 			"content" => "students/new_order",
 			"student" => $student,
-			"promos" => $this->promos
+			"promos" => $this->promos_arr
 		));
 	}
 
@@ -64,7 +64,7 @@ class Students extends CI_Controller {
 	public function validate_order()
 	{
 		$this->load->library('form_validation');
-		$this->form_validation->set_rules('total_invited', 'Total Invited', 'required|greater_than[0]');
+		$this->form_validation->set_rules('total_invited', 'Total Invited', 'required|greater_than[0]|callback_invited_number['.$this->input->post("promo_id").']');
 		if ($this->form_validation->run()) {
 			$new_order = array(
 				"student_id" => $this->input->post('student_id'),
@@ -74,6 +74,17 @@ class Students extends CI_Controller {
 			$this->utilities_db->generic_insert("promo_orders", $new_order );
 			$this->session->set_flashdata('insert_msg', 'New Promo Order Inserted');
 			redirect('students/');	
+		}
+	}
+
+	public function invited_number( $total_invited, $promo_id )
+	{
+		if( $this->promos_arr[$promo_id] != "Paquete A" && $total_invited < 4 ){
+			$this->form_validation->set_message('invited_number', "El paquete '".$this->promos_arr[$promo_id].
+				"' debe tener al menos 4 invitados.");
+			return false;
+		} else {
+			return true;
 		}
 	}
 
--- application/views/students/new_order.php
+++ application/views/students/new_order.php
@@ -5,10 +5,10 @@
 	<h4><?php echo $title; ?></h4>
 	<?php echo validation_errors("<div class='alert alert-danger'>","</div>"); ?>
 	<?php echo form_open('students/new_order/'.$student->id, 'class="form"', array("student_id" => $student->id )); ?>
-		<label>Promo: </label><?php echo form_dropdown('promo_id', $promos ); ?>
-		<label>Total invited: </label>
-		<input type='number' name="total_invited" type="number" min="1" max="15" value="1">
+		<label>Paquete: </label><?php echo form_dropdown('promo_id', $promos ); ?>
+		<label>Total invitados: </label>
+		<input type='number' name="total_invited" type="number" min="1" max="25" value="1">
 		<br>
-		<?php echo form_submit('submit', 'New Order', 'class="btn btn-primary btn-small"'); ?>
+		<?php echo form_submit('submit', 'Crear pedido', 'class="btn btn-primary btn-small"'); ?>
 	<?php echo form_close() ?>
 </div>
