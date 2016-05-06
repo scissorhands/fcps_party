eee3d3fd38e281f5d2340f7eaa9fb22bb9400e58
scissorhands <edward_beats>
2016-05-06 18:20:44 -0500

    progress bar 2

---
+++ application/views/tools/progress_bar.php
@@ -0,0 +1,5 @@
+<div class="progress">
+  <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $percentage; ?>%">
+    <span class="sr-only"><?php echo $percentage; ?>% Complete</span>
+  </div>
+</div>
