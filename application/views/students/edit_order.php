90792ef5050ddb0534f60a20a2c78d83e7599bb3
scissorhands <edward_beats>
2016-05-24 21:02:29 -0500

    no invited people limit

--- application/views/students/edit_order.php
+++ application/views/students/edit_order.php
@@ -7,7 +7,7 @@
 	<?php echo form_open('promo_orders/edit_promo_order/'.$order->student_id, 'class="form"', array("student_id" => $order->student_id )); ?>
 		<label>Paquete: </label><?php echo form_dropdown('promo_id', $promos, $order->promo_id ); ?>
 		<label>Total invitados: </label>
-		<input type='number' name="total_invited" type="number" min="1" max="25" value="<?php echo $order->invited; ?>">
+		<input type='number' name="total_invited" type="number" min="1" value="<?php echo $order->invited; ?>">
 		<br>
 		<?php echo form_submit('submit', 'Guardar', 'class="btn btn-primary btn-small"'); ?>
 	<?php echo form_close() ?>
